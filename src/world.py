28b9fa1afaeaa2576fc14600ceab4b548a640bc2
cesse <cesse@cesse-laptop.(none)>
2011-12-19 21:56:05 +0200

    world

--- src/world.py
+++ src/world.py
@@ -16,28 +16,36 @@ class World:
         self.currentRoom = self.createStartRoom()    
 
     def createStartRoom(self):
-        startRoom = TileMap(self.tileSize, 100, 20)
+        mapSizex = 100
+        mapSizey = 100
+        startRoom = TileMap(self.tileSize ,mapSizex ,mapSizey)
         backWall = startRoom.addTileLayer('backWall')
         bg = startRoom.addTileLayer('background')
         startRoom.addEntityLayer('entities')
         startRoom.addEntityLayer('player')
         startRoom.addTileLayer('foreground')
 
-        for i in range(0,3):
-            self.mapBuilder.addBlock(bg, "stone", i * 4, 12)
-        for i in range(4,6):
-            self.mapBuilder.addBlock(bg, "stone", (i * 4)-1, 11)
-        for i in range(7,9):
-            self.mapBuilder.addBlock(bg, "stone", (i * 4)+3, 16)
-        for i in range(10,13):
-            self.mapBuilder.addBlock(bg, "stone", (i * 4), 15)
-        for i in range(13,15):
-            self.mapBuilder.addBlock(bg, "stone", (i * 4)+1, 14)
-        for i in range(15,19):
-            self.mapBuilder.addBlock(bg, "stone", (i * 4)+1, 16)     
+        #reunat
+        for x in range(0,mapSizex ):
+            for y in range(0,2):
+                self.mapBuilder.addBlock(bg, "stone", x * 4, y * 4)
 
-        for x in range(0, 20):
-            for y in range(0,20):
+        
+
+       
+        for i in range(4,6):
+            self.mapBuilder.addBlock(bg, "stone", (i * 4)-1, 16)
+        for i in range(7,9):
+            self.mapBuilder.addBlock(bg, "stone", (i * 4)+3, 21)
+        for i in range(10,13):
+            self.mapBuilder.addBlock(bg, "stone", (i * 4), 20)
+        for i in range(13,15):
+            self.mapBuilder.addBlock(bg, "stone", (i * 4)+1, 19)
+        for i in range(15,19):
+            self.mapBuilder.addBlock(bg, "stone", (i * 4)+1, 21)     
+
+        for x in range(0, mapSizex):
+            for y in range(0,mapSizey):
                 self.mapBuilder.addBlock(backWall, "wall", x * 4, y * 4)     
 
         return startRoom
